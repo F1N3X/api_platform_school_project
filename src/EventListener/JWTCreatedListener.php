893b56260eef77dd188ccd3701f92a6b6092c1ef
F1N3X <quartzman92@gmail.com>
2025-03-21 12:25:07 +0100

    avoid to edit different user password

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -21,10 +21,10 @@ class JWTCreatedListener
         // Récupération du payload
         $payload = $event->getData();
         // Récupération de l'utilisateur concerné
-        $user = $this->userRepository->findOneByEmail($payload['username']);
+        $user = $this->userRepository->findOneByIdentifiant($payload['username']);
  
         // Ajout d'une clé 'firstname' au payload
-        $payload['firstname'] = $user->getFirstname();
+        $payload['firstname'] = $user->getPrenom();
  
         // Mise à jour du payload
         $event->setData($payload);
